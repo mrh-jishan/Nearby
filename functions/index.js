9c0526e26356787a343e20be1d3105cf19022aeb
robin-hassan <robiul.hassan12102@gmail.com>
2021-01-10 03:33:14 +0800

    added firebase functions for createUser

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,35 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.createUser = functions.auth.user().onCreate((user) => {
+    return admin.firestore().collection('users').doc(user.uid)
+        .set({
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+            email: user.email,
+            disabled: user.disabled,
+            emailVerified: user.emailVerified,
+            phoneNumber: user.phoneNumber,
+        }).then(res => {
+            console.log('User created result:', res);
+            return res;
+        }).catch(err => {
+            console.log('Error in creating: ', err);
+            return err;
+        });
+});
+
+
+exports.deleteUser = functions.auth.user().onDelete((user) => {
+    return admin.firestore().collection('users').doc(user.uid).delete().then(res => {
+        console.log('User deleted result:', res);
+        return res;
+    }).catch(err => {
+        console.log('Error in deleting: ', err);
+        return err;
+    });
+});
